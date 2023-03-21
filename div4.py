56ea16b2916a4a01dc45321eba39c2c0e9f74bf7
vamshikrishna1997 <86228152+vamshikrishna1997@users.noreply.github.com>
2023-03-21 14:50:18 +0530

    Update div4.py

--- div4.py
+++ div4.py
@@ -1,8 +1,6 @@
 
 def isdiv4(number):
 
-#    number = "0" + number
-#    print(number)
     tendigit = number[-2]
     onedigit = number[-1]
 
@@ -16,39 +14,5 @@ def isdiv4(number):
 if __name__ == "__main__":
     number = input("Enter a number:")
     print(f"{number} is {'' if isdiv4(number) else 'not'} divisble by 4")
-
-
-##****************************************************************************************
-
-# def isdiv4(number):
-#
-#   # if len(number) > 1:
-#   #    print(number)
-#
-#     tendigit = int(number[-2])
-#     onedigit = int(number[-1])
-#
-#     if tendigit in [0,2,4,6,8] and onedigit in [0,4,8]:
-#         return True
-#     elif tendigit [1,3,5,7,9] and onedigit in [2,6]:
-#         return True
-#
-#     else:
-#         return False
-#
-# if __name__ == "__main__":
-#     number = input("Enter a number:")
-#     print(f"{number} is {'' if isdiv4(number) else 'not'} divisble by 4")
-
-
-
-
-
-
-
-
-
-
-
 
 
